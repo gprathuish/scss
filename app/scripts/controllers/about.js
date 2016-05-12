fe051b17199c18342b5b599fa1240ae615ea59ca
gprathuish <gprathuis@gmail.com>
2016-05-12 20:09:25 +0530

    Prathuish - added Project files

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name scssApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the scssApp
+ */
+angular.module('scssApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name scssApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the scssApp
+ */
+angular.module('scssApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
